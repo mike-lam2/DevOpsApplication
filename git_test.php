76c65e506ddf277b7b34728644e284595c31e32b
mike-lam2 <mike-lam2@example.com>
2016-03-04 15:37:21 -0500

    .

--- git_test.php
+++ git_test.php
@@ -1,13 +1,15 @@
-<?php 
+<?php
+echo "received a push"; 
 try
 {
   $payload = json_decode($_REQUEST['payload']);
 }
 catch(Exception $e)
 {
+  echo "payload cannot be decoded";
   exit(0);
 }
-
+echo "payload decoded";
 //log the request
 file_put_contents('logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
 
@@ -17,3 +19,4 @@ if ($payload->ref === 'refs/heads/master')
   // path to your site deployment script
   exec('/var/www/html/DevOpsApplication/build.sh');
 }
+echo "push processed sucessful";
