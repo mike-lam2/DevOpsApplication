feb4a7ead083c879dd71aff23687521bdf3d48e3
mike-lam2 <author@example.com>
2016-03-04 18:20:24 -0500

    .

--- git_test.php
+++ git_test.php
@@ -1,15 +1,15 @@
 <?php
-echo "received a push<p>"; 
+echo "received a push\n"; 
 try
 {
   $payload = json_decode($_REQUEST['payload']);
 }
 catch(Exception $e)
 {
-  echo "payload cannot be decoded<p>";
+  echo "payload cannot be decoded\n";
   exit(0);
 }
-echo "payload decoded<p>";
+echo "payload decoded\n";
 //log the request
 file_put_contents('logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
 file_put_contents('/var/www/html/DevOpsApplication/github.txt', print_r($payload, TRUE), FILE_APPEND);
@@ -17,7 +17,8 @@ file_put_contents('/var/www/html/DevOpsApplication/github.txt', print_r($payload
 
 if ($payload->ref === 'refs/heads/master')
 {
+  echo "executing\n";
   // path to your site deployment script 
   exec('/var/www/html/DevOpsApplication/build.sh');
 }
-echo "push processed sucessful<p>"; 
+echo "push processed sucessful\n"; 
