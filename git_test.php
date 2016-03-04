0d8be24aafb97419adcd90342f95545a4acc5a78
mike-lam2 <author@example.com>
2016-03-04 17:41:50 -0500

    tst hook

--- git_test.php
+++ git_test.php
@@ -17,7 +17,7 @@ file_put_contents('/var/www/html/DevOpsApplication/github.txt', print_r($payload
 
 if ($payload->ref === 'refs/heads/master')
 {
-  // path to your site deployment script
+  // path to your site deployment script 
   exec('/var/www/html/DevOpsApplication/build.sh');
 }
 echo "push processed sucessful<p>";
