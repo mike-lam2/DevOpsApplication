2001037efb0d29de9f51a8f2de6cc64978809b30
mike-lam2 <author@example.com>
2016-03-05 14:54:51 -0500

    fix git reset hard

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 cd /var/www/html/DevOpsApplication
 git fetch --all
-git reset --hard origin/master
+git reset --hard
 chmod a+x ../DevOpsApplication/*.sh
 git show --abbrev-commit --no-notes --format=full --no-patch 2>&1 >> WebContent/WEB-INF/version.txt
 
