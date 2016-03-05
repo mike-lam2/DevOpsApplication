1b02ba069a62c30fccb887c942eac34273500967
mike-lam2 <author@example.com>
2016-03-04 23:00:02 -0500

    .

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 cd /var/www/html/DevOpsApplication
-sudo git pull
+sudo git pull --hard
 sudo chmod -R a+r ../DevOpsApplication
 sudo chmod -R a+w ../DevOpsApplication
 sudo chmod a+x ../DevOpsApplication/*.sh"
