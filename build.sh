5ac9e5590878d32c62e563506ae65649603d4b2f
mxl125 <michel.lamarche@cra-arc.gc.ca>
2016-03-05 09:33:24 -0500

    clean up build.sh now assum all user apache

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 cd /var/www/html/DevOpsApplication
 git fetch --all
 git reset --hard origin/master
-chmod -R a+r ../DevOpsApplication
-chmod -R a+w ../DevOpsApplication
 chmod a+x ../DevOpsApplication/*.sh
-#11:52 yay it works
+
 
