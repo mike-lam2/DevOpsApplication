0276c31205ce8f2b0f64dd9d66ab7e2230c96d20
mike-lam2 <author@example.com>
2016-03-05 13:04:33 -0500

    testing version.html

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ cd /var/www/html/DevOpsApplication
 git fetch --all
 git reset --hard origin/master
 chmod a+x ../DevOpsApplication/*.sh
-
+git show --abbrev-commit --no-notes --format=full --no-patch 2>&1 > WebContent/WEB-INF/version.html
 
